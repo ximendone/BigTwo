902ea74ed6662e295adbb91eb6fc7224f14bad07
k34425707 <b102145@gmail.com>
2022-07-27 18:21:57 +0800

    modify config and update register page

---
+++ server/page/register.php
@@ -0,0 +1,21 @@
+<?php
+echo '<form>
+<div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label textcolor">E-mail</label>
+    <input type="email" class="form-control" id="email">
+  </div>
+<div class="mb-3">
+  <label for="exampleInputEmail1" class="form-label textcolor">Username</label>
+  <input type="text" class="form-control" id="username" aria-describedby="emailHelp">
+</div>
+<div class="mb-3">
+  <label for="exampleInputPassword1" class="form-label textcolor">Password</label>
+  <input type="password" class="form-control" id="password">
+</div>
+<div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label textcolor">Password Check</label>
+    <input type="password" class="form-control" id="passwordcheck">
+</div>
+<button type="submit" class="btn btn-primary">註冊</button>
+<button type="submit" class="btn btn-primary">返回</button>
+</form>';
