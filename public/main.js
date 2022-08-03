d06ccf6233369cd5f2d7adee11ba67eed8abae6b
akito <61858093+akito1129@users.noreply.github.com>
2022-08-03 17:41:33 +0800

    feat:連接登入API

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var apiroot = "../server/api/"
+var nickname = ""
 function to_register() {
     $("#login").empty();
     $("#login").append('<form><div class="mb-3"><label for="exampleInputEmail1" class="form-label textcolor">Username</label><input type="text" class="form-control" id="username" aria-describedby="emailHelp"></div><div class="mb-3"><label for="exampleInputEmail1" class="form-label textcolor">E-mail</label><input type="email" class="form-control" id="email" aria-describedby="emailHelp"></div><div class="mb-3"><label for="exampleInputEmail1" class="form-label textcolor">Password</label><input type="password" class="form-control" id="password" aria-describedby="emailHelp"></div><div class="mb-3"><label for="exampleInputPassword1" class="form-label textcolor">Password check</label><input type="password" class="form-control" id="passwordcheck"></div><input type="button" class="btn btn-primary" onclick="do_register()" value="註冊"></input><input type="button" class="btn btn-primary" onclick="to_index()" value="返回"></input></form>');
@@ -9,23 +10,15 @@ function to_forgetpassword() {
 }
 function to_index() {
     $("#login").empty();
-    $("#login").append('<form><div class="mb-3"><label for="exampleInputEmail1" class="form-label textcolor">Username</label><input type="text" class="form-control" id="username" aria-describedby="emailHelp"></div><div class="mb-3"><label for="exampleInputPassword1" class="form-label textcolor">Password</label><input type="password" class="form-control" id="password"></div><div class="mb-3 form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label textcolor" for="exampleCheck1">Check me out</label></div><button type="submit" class="btn btn-primary">登入</button><input type="button" class="btn btn-primary" onclick="to_register()" value="註冊"></input><input type="button" class="btn btn-primary" onclick="to_forgetpassword()" value="忘記密碼"></input></form>');
+    $("#login").append('<form><div class="mb-3"><label for="exampleInputEmail1" class="form-label textcolor">Username</label><input type="text" class="form-control" id="username" aria-describedby="emailHelp"></div><div class="mb-3"><label for="exampleInputPassword1" class="form-label textcolor">Password</label><input type="password" class="form-control" id="password"></div><div class="mb-3 form-check"><input type="checkbox" class="form-check-input" id="exampleCheck1"><label class="form-check-label textcolor" for="exampleCheck1">Check me out</label></div><input type="button" class="btn btn-primary" onclick="do_login()" value="登入"></input><input type="button" class="btn btn-primary" onclick="to_register()" value="註冊"></input><input type="button" class="btn btn-primary" onclick="to_forgetpassword()" value="忘記密碼"></input></form>');
+}
+function to_lobby() {
+    $("#login").empty();
+    $("#login").append('');
 }
 
 
 function do_register() {
-    jQuery.validator.setDefaults({
-        debug: true,
-        success: "valid"
-      });
-    $("#login").validate({
-        rules:{
-            password: "required",
-            passwordcheck: {
-                equalTo: "#password"
-            }
-        }
-    })
     $.ajax({
     type: 'POST',
     url: apiroot + 'register.php',
@@ -33,7 +26,7 @@ function do_register() {
         username: $("#username"). val(),
         password: $("#password"). val(),
         email: $("#email"). val(),
-        nickname: "Nick"
+        nickname: $("#username"). val()
     },
     success: function(response) {
         console.log(response);
@@ -49,4 +42,31 @@ function do_register() {
         }
     }
 })
+}
+
+function do_login() {
+    console.log($("#username"). val());
+    console.log($("#password"). val());
+    $.ajax({
+        type: 'POST',
+        url: apiroot + 'login.php',
+        data: {
+            username: $("#username"). val(),
+            password: $("#password"). val(),
+        },
+        success: function(response) {
+            console.log(response);
+            var obj = jQuery.parseJSON(response);
+            console.log(obj);
+            if (obj.success == true){
+                alert("登入成功");
+                nickname = obj.nickname;
+                to_lobby();
+            }
+            else {
+                if (obj.reason == "data"){alert("帳號或密碼錯誤");}
+                else {alert("資料不齊全");}
+            }
+        }
+    })
 }
